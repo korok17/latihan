dfb4a2de69588777649b58e889bcc1d28cba1742
korok <muhammadzikri598@gmail.com>
2019-04-10 14:07:20 +0700

    new menu management

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -5,7 +5,7 @@ class Admin extends CI_Controller {
 
     public function index()
     {
-        $data['title'] = 'Dasbord';
+        $data['title'] = 'Dashboard';
         $data['user'] = $this->db->get_where('user', ['email' =>$this->session->userdata('email')])->row_array();
         
         $this->load->view('templates/header', $data);
